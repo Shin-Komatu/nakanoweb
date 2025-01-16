d5b52208c41fced5a1068d9e853e796cc298d6c0
kom <s.k.july.7@icloud.com>
2025-01-16 16:53:31 +0900

    粒子の動き

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function createParticle() {
     const particle = document.createElement('div');
-    particle.classList.add('particle', 'particle-1');
+    particle.classList.add('particle');
     document.body.appendChild(particle);
     
     // ランダムな位置とタイミングで生成
@@ -17,4 +17,3 @@ function createParticle() {
 
 // 定期的に粒子を生成
 setInterval(createParticle, 200); // 0.2秒ごとに生成
-
